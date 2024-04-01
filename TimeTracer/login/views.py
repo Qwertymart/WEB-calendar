47bba2f347f7da97c8ad91ebd0570a1f32e388a6
Qwertymart <tremeldima4@gmail.com>
2024-04-01 22:22:51 +0300

    Добавил хеширование пароля на этапе регистрации. При входе пароль хешируется и сравнивается с хешем, заложенным в БД.

--- TimeTracer/login/views.py
+++ TimeTracer/login/views.py
@@ -1,10 +1,8 @@
 import logging
-
 from django.shortcuts import render, redirect
 from register.forms import UserForm
 from register.models import users
-
-logger = logging.getLogger(__name__)
+import hashlib
 
 def login_views(request):
     error = ''
@@ -13,6 +11,7 @@ def login_views(request):
         if form.is_valid():
             name = request.POST.get('name')
             password = request.POST.get('password')
+            password = hashlib.sha256(password.encode()).hexdigest()
             user = users.objects.filter(name=name).first()
             if user is not None and user.password == password:
                 return redirect('home/')
@@ -20,6 +19,7 @@ def login_views(request):
                 error = 'Неверное Имя или Пароль'
     else:
         form = UserForm()
+        
     data = {
         'error': error,
         'form': form
--- TimeTracer/register/views.py
+++ TimeTracer/register/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import UserForm
 from .models import users
+import hashlib
 
 
 def register(request):
@@ -8,19 +9,21 @@ def register(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         password = request.POST.get('password')
-        password2 = request.POST.get('password2')
+        password_repeat = request.POST.get('password_repeat')
         name = request.POST.get('name')
         if form.is_valid():
             if not (users.objects.filter(name=name).exists()):
-                if password == password2:
-                    form.save()
+                if password == password_repeat:
+                    password = hashlib.sha256(password.encode()).hexdigest()
+                    user = users(name=name, password=password)
+                    user.save()
                     return redirect('home')
                 else:
-                    error = "Пароли отличаются друг от друга"
+                    error = 'Пароли отличаются друг от друга'
             else:
-                error = "Данное имя пользователя уже существует"
+                error = 'Данное имя пользователя уже существует'
         else:
-            error = "Неверно введенная форма"
+            error = 'Неверно введенная форма'
 
     form = UserForm(request.POST)
 
