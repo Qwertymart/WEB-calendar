ae42ce27664ba8f5959b3f351f7320d88389619d
Qwertymart <tremeldima4@gmail.com>
2024-04-20 09:52:18 +0300

    Добавлен сам календарь с выбором месяца

--- TimeTracer/events/views.py
+++ TimeTracer/events/views.py
@@ -1,7 +1,10 @@
+# views.py
 from django.shortcuts import render, redirect
 from .forms import EventForm
 from .models import events as Event
 from django.utils import timezone
+from datetime import datetime
+from calendar import monthrange
 
 def events(request):
     if request.method == 'POST':
@@ -11,20 +14,45 @@ def events(request):
             form = EventForm(request.POST)
             if form.is_valid():
                 form.save()
-                return redirect('events')
+                return redirect('/events')
     else:
         form = EventForm()
-
-    layout = request.GET.get('layout')
-    month = request.GET.get('month')
-    week = request.GET.get('week')
 
     current_year = timezone.now().year
     current_month = timezone.now().month
 
-    if layout == 'week':
-        events = Event.objects.filter()
-    else:
-        events = Event.objects.filter()
+    selected_month = int(request.GET.get('selected_month', current_month))
 
-    return render(request, 'events/events.html', {'form': form, 'events': events, 'current_month': current_month, 'current_year': current_year})
+    # Calculate the number of days in the selected month
+    _, num_days = monthrange(current_year, selected_month)
+
+    # Get the first day of the selected month
+    first_day = datetime(current_year, selected_month, 1)
+
+    # Calculate the weekday of the first day (0 - Monday, 6 - Sunday)
+    first_weekday = first_day.weekday()
+
+    # Calculate the number of days to pad before the first day of the month in the calendar
+    num_padding_days = (first_weekday) % 7
+
+    # Generate a list of days for the selected month
+    days_of_month = []
+    week = []
+    for i in range(num_padding_days):
+        week.append(None)
+    for day in range(1, num_days + 1):
+        week.append(datetime(current_year, selected_month, day))
+        if len(week) == 7:
+            days_of_month.append(week)
+            week = []
+    if week:
+        while len(week) < 7:
+            week.append(None)
+        days_of_month.append(week)
+
+    # Retrieve events for the selected month
+    events = Event.objects.filter(date_start__year=current_year, date_start__month=selected_month)
+
+    return render(request, 'events/events.html',
+                  {'events': events, 'current_month': current_month, 'current_year': current_year,
+                   'days_of_month': days_of_month})
