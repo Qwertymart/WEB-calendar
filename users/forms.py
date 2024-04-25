896d595a14998dbc9d0da793c1aebcaa607dc8ff
IvKorolev <mrvanivan@mail.ru>
2024-04-25 18:05:22 +0300

    Полностью доделана регистрация и вход, ветка готова к merge-у с веткой events

--- users/forms.py
+++ users/forms.py
@@ -1,37 +1,36 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 
 
 class LoginUserForm(AuthenticationForm):
-    #username = forms.CharField(label="Логин")
-    #password = forms.CharField(label="Пароль")
+    username = forms.CharField(label="Логин", widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password = forms.CharField(label="Пароль", widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
     class Meta:
         model = get_user_model()
         fields = ('username', 'password')
 
 
-class RegisterUserForm(forms.ModelForm):
-    username = forms.CharField(label="Логин")
-    password = forms.CharField(label="Пароль", widget=forms.PasswordInput())
-    password2 = forms.CharField(label="Повтор пароля", widget=forms.PasswordInput())
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label="Логин", widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label="Пароль", widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label="Повтор пароля", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = get_user_model()
-        fields = ('first_name', 'last_name', 'username', 'password', 'password2')
+        fields = ('first_name', 'last_name', 'username', 'password1', 'password2')
 
     labels = {
         'first_name': 'Имя',
         'last_name': 'Фамилия',
     }
 
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Пароли не совпадают!')
-        return cd['password']
+    widgets = {
+        'first_name': forms.TextInput(attrs={'class': 'form-input'}),
+        'last_name': forms.TextInput(attrs={'class': 'form-input'}),
+    }
 
     def clean_username(self):
         username = self.cleaned_data['username']
