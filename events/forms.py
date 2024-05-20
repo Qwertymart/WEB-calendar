78f7125341ac96fcdf985c0bdf6e3b38cf6ead5e
Qwertymart <tremeldima4@gmail.com>
2024-05-20 20:49:54 +0300

    Добавил возможность выбора цвета для события

--- events/forms.py
+++ events/forms.py
@@ -6,12 +6,13 @@ from .models import Event
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ['name', 'date_start', 'time_start', 'date_finish', 'time_finish', 'description']
+        fields = ['name', 'date_start', 'time_start', 'date_finish', 'time_finish', 'description', 'color']
         widgets = {
             'date_start': forms.DateInput(attrs={'class': 'datepicker'}),
             'time_start': forms.TimeInput(attrs={'class': 'timepicker'}),
             'date_finish': forms.DateInput(attrs={'class': 'datepicker'}),
             'time_finish': forms.TimeInput(attrs={'class': 'timepicker'}),
+            'color': forms.TextInput(attrs={'type': 'color'}),
         }
 
 
